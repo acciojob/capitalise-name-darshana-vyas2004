633f50291ff832de695e562720b000575b700ee5
darshana-vyas2004 <darshanavyas2021@gmail.com>
2025-05-14 08:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function convertToUppercase() {
+  let input = document.getElementById("fname");
+  input.value = input.value.toUpperCase();
+}
+document.getElementById("fname").addEventListener("blur", convertToUppercase);
